fcc7773b1253a22bba0dc207dd4b9ca98de6d2e4
zorozuro <yasinnew72@gmail.com>
2025-03-14 20:05:54 +0530

    Move @vitejs/plugin-react to dependencies and update build script

--- backend/config/env.js
+++ backend/config/env.js
@@ -1,17 +1,21 @@
 import dotenv from "dotenv";
 import path from "path";
 
-const envPath = path.resolve(process.cwd(), ".env");
-console.log(`Loading environment variables from : ${envPath}`);
+// Load environment variables from .env file in development
+if (process.env.NODE_ENV !== "production") {
+  const envPath = path.resolve(process.cwd(), ".env");
+  console.log(`Loading environment variables from: ${envPath}`);
 
-const res = dotenv.config({ path: envPath });
+  const res = dotenv.config({ path: envPath });
 
-if (res.error) {
-  console.error("Error loading environment variables: ", res.error);
-  process.exit(1);
-} else {
-  console.log("Environment variables loaded successfully");
+  if (res.error) {
+    console.error("Error loading environment variables: ", res.error);
+    process.exit(1);
+  } else {
+    console.log("Environment variables loaded successfully");
+  }
 }
 
+// Export environment variables
 export const { MONGO_URI, PORT, NODE_ENV } = process.env;
 console.log("NODE_ENV:", NODE_ENV); // Debug log to check the value
